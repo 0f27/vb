281f65f9e54e0bfeba5689c1129ddf104eb138fb
Petr <petr@duck.com>
2024-08-26 21:39:55 +0300

    ensuring that ~/.local/bin added to PATH

--- setup.sh
+++ setup.sh
@@ -4,3 +4,13 @@ mkdir -p "$HOME/.local/bin"
 for s in v*; do
   ln -s "$(pwd)/$s" "$HOME/.local/bin/$s"
 done
+
+if ! grep -q '.local/bin' $HOME/.zshrc; then
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.zshrc
+fi
+if ! grep -q '.local/bin' $HOME/.bashrc; then
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc
+fi
+if ! grep -q '.local/bin' $HOME/.config/fish/config.fish; then
+  echo "set -a fish_user_paths $HOME/.local/bin" >> $HOME/.config/fish/config.fish
+fi
